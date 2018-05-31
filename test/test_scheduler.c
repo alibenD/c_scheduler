9037e051c8892ede472d00e6748547f3e8771c4b
alibenD <aliben.develop@gmail.com>
2018-05-31 17:56:34 +0800

    Updated test instance for scheduler

--- test/test_scheduler.c
+++ test/test_scheduler.c
@@ -4,7 +4,7 @@
   * @version: v0.0.1
   * @author: aliben.develop@gmail.com
   * @created_date: 2018-05-30 20:15:28
-  * @last_modified_date: 2018-05-31 14:18:41
+  * @last_modified_date: 2018-05-31 17:29:56
   * @brief: TODO
   *-----------------------------------------------*/
 
@@ -16,7 +16,19 @@
 int main(int argc, char** argv)
 {
   printf("Test!\n");
+  const int MAX_NUMBER_TASKS = 10;
+  Task task_list[MAX_NUMBER_TASKS];
   Task_MGR task_mgr;
+  init_task_manager(&task_mgr, task_list, MAX_NUMBER_TASKS);
+
+  Task task1;
+  Task task2;
+
+  monitor_task_manager(&task_mgr);
+
+  add_new_task(&task_mgr, &task1);
+  monitor_task_manager(&task_mgr);
+  add_new_task(&task_mgr, &task2);
   monitor_task_manager(&task_mgr);
   return 0;
 }
